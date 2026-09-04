26a4fb722a136dd7f85288f46fdb0023a8c5c1db
zfassoth <zfassoth@gmail.com>
2026-09-03 19:06:38 -0500

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="dealflow-v44-pwa";
+const CACHE="dealflow-v46-brand";
 const STATIC=["/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png","/icons/apple-touch-icon.png"];
 
 self.addEventListener("install",event=>{
